0a453619d8ce0afddc3bd26e8faa84ad768a0772
luciozz <lucioz.14@hotmail.com>
2023-10-15 23:40:43 -0300

    before firebase section

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import './navigation.styles.scss';
+
+import CrwnLogo from "../../assets/crown.svg?react";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to='/'>
+          <CrwnLogo className='logo'/>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to='shop'>
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet/>
+    </Fragment>
+  );
+};
+
+export default Navigation;
